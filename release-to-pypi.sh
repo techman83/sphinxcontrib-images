8ac842ca053a139bba3dd6e712ca485f8f9441fe
Jonas Camillus Jeppesen <jonascj@jonascj.dk>
2021-04-28 09:40:11 +0200

    Updated release instructions

--- release-to-pypi.sh
+++ release-to-pypi.sh
@@ -5,8 +5,6 @@ V2=$(sed -nE "s/^.*version.*=.*([0-9]+\.[0-9]+\.[0-9]+(\.[a-z0-9]+)?).*$/\1/p" s
 SUB=$(git submodule status sphinxcontrib_images_lightbox2/lightbox2 | cut -c1)
 
 printf "\n## RELEASE SPHINXCONTRIB-IMAGES TO PYPI ##\n\n"
-printf "Refer to the comments and commented out commands\n"
-printf "in this script for one possible release procedure.\n\n"
 
 printf "# VERSION CHECK #\n"
 printf "setup.py: %s\n" "$V1"
@@ -25,32 +23,79 @@ else
     echo "SUCCESS: lightbox2 submodule initialized"
 fi
 
+read -p "Press any key to proceed to instructions ..." -n1 -s
+
+cat <<STEPS
 
 
-## Prerequisites
-## - Bump version in setup.py
-## - Bump version in sphinxcontrib/images.py
-## - Make sure the lightbox2 submodule is clone
-##   (git submodule update --init --recursive)
+# 1. Clone lightbox2 submodule:
+git submodule update --init --recursive
 
 
-## Create a virtual environment for twine installation
-# python -m venv venv
-# source venv/bin/activate
-# pip install twine
+# 2. Create a virtual environment and install twine
+python -m venv venv-twine 
+source venv-twine/bin/activate
+pip install twine
+
+# 3. Test release to TestPyPI
+ - Requires registration at https://test.pypi.org/account/register/ 
+
+ - Bump version by editing setup.py and sphinxcontrib/images.py 
+   e.g. from 0.9.2 to 0.9.3.pre1 (use .pre2 second test release etc.) 
+
+ - Remove old distributions/builds
+rm -r dist/
+
+ - Build the distribution:
+python setup.py sdist bdist_wheel
+
+ - Upload to TestPyPI
+twine upload -r testpypi dist/*
 
 
-## Create the distribution
-# python setup.py sdist bdist_wheel
+# 4. Test the TestPyPI-release
+ - Make a new virtual environment
+python -m venv venv-0.9.3.pre1
+
+ - Install the prerelease (see note [1])
+pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple sphinxcontrib-images
+
+ - Verify the version installed:
+pip freeze | grep sphinxcontrib-images
+
+ - Verify the prereleased package can build the package docs in docs/
+cd docs/
+make html 
+
+ - If anything fails repeat step 3. and 4.
 
 
-## Upload to TestPyPI to verify things look right
-## Add '.preN' to the release number when testing (e.g. '0.9.3.pre1')
-## https://test.pypi.org/account/register/ 
-# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# 5. Final test release to TestPyPI
+ - Repeat step 3. and 4. but without .preN in the version number,
+   e.g. change 0.9.3.pre1 to 0.9.3
 
 
-## Upload to PyPI
-## Remember to remove '.preN' from the version before rebuilding and uploading
-## Requires user with maintainer status on the package/project
-# twine upload dist/*
+# 6. Release to PyPI
+ - Requires user with maintainer status on the package
+   https://pypi.org/project/sphinxcontrib-images/
+
+ - Upload to PyPI
+twine upload dist/*
+
+# 7. Commit released version, tag and push
+git add setup.py sphinxcontrib/images.py
+git commit -m "Version bumped to a.b.c"
+git tag -a a.b.c -m "release a.b.c"
+git push origin master
+
+
+[1] Version 0.9.3.pre1 in setup.py is converted to 0.9.3rc1 in TestPyPI
+    so to install a specific version of the prerelese use 
+    'sphinxcontrib-images==0.9.3rc1' with pip.
+
+    If you release to TestPyPI in this order: 0.9.3.pre1 - 0.9.3.pre2 - 0.9.3
+    then you should be able to test all three versions by simply installing the 
+    package without stating the version since 0.9.3.pre2 > 0.9.3.pre1 and 
+    0.9.3 > 0.9.3.pre2
+    
+STEPS
